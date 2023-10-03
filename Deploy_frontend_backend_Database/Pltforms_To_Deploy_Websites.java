5b528971342e8c81300d037b9c7de5b841c85921
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-03 10:49:44 +0300

    Create Pltforms_To_Deploy_Websites.java

---
+++ Deploy_frontend_backend_Database/Pltforms_To_Deploy_Websites.java
@@ -0,0 +1,59 @@
+There are many platforms available for deploying and hosting full-stack web apps. Here are some of the most popular options:
+
+1. Railway
+- Offers automated Git-based deploys via GitHub
+- Supports various apps, including static sites, background workers, cron jobs, and databases
+- Provides one-click quick start deployments
+- Offers fixed expenses based on plan
+
+2. Heroku
+- Supports many languages, including JavaScript, Ruby, Python, and PHP
+- Provides git integration to make the deployment process easy
+- Offers a generous free tier
+
+3. Render
+- Provides public REST APIs to manage your render services over HTTP
+- Offers a free tier with 100 GB of data transfer and 100 GB of storage
+- Provides automatic SSL certificates for custom domains
+
+4. Webapp.io
+- Hosts full-stack web projects worldwide without managing any complex infrastructure
+- Offers a free tier with 1 GB of storage and 1 GB of bandwidth per month
+- Provides automatic SSL certificates for custom domains
+
+5. Netlify
+- Offers an amazing service for hosting static websites
+- Provides unlimited projects hosting
+- Offers a generous free tier
+
+6. Vercel
+- Provides serverless architecture to set up infrastructure for deployment
+- Offers a generous free tier
+- Supports both backend and frontend applications
+
+7. Deta Cloud
+- Provides a build and deployment platform for full-stack web apps
+- Offers a free tier with 3 GB of storage and 10 GB of bandwidth per month
+- Provides automatic SSL certificates for custom domains
+
+8. AWS Elastic Beanstalk
+- Provides a fully managed service for deploying and scaling web applications
+- Offers a free tier with 750 hours of Amazon EC2 usage per month
+
+9. DigitalOcean App Platform
+- Provides a fully managed platform for deploying and scaling web apps
+- Offers a free tier with 2 GB of memory and 3 static sites
+
+10. Google Cloud Run
+- Provides a fully managed platform for deploying and scaling containerized apps
+- Offers a free tier with 2 million requests per month
+
+These are just a few of the many platforms available for deploying and hosting full-stack web apps. The specific platform you choose will depend on your needs, budget, and technical expertise.
+
+Citations:
+[1] https://webdevwithseb.com/2022/03/03/the-3-best-full-stack-hosting-platforms-2022/
+[2] https://www.amplifyre.com/articles/how-deploy-full-stack-app-web
+[3] https://webapp.io/hosting
+[4] https://akashrajpurohit.com/blog/top-free-services-to-deploy-full-stack-applications/
+[5] https://stackdiary.com/free-hosting-for-developers/
+[6] https://bugfender.com/blog/10-best-app-deployment-platforms/
